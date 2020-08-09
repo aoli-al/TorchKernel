ea6c49383ac255370e42a8e9b527ba01e750f5ce
Leeleo3x <leeleo3x@gmail.com>
2020-08-09 14:28:34 -0400

    update.

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
         extra_compile_args={'cxx': [],
                             'nvcc': nvcc_args
                                      },
-        include_dirs = ['/home/hao01/torch_extension/lib/python3.6/site-packages/torch/include', '/home/hao01/pytorch/aten/src'],
-        library_dirs = ['/home/hao01/torch_extension/lib/python3.6/site-packages/torch/lib'],
+        #  include_dirs = ['/home/hao01/torch_extension/lib/python3.6/site-packages/torch/include', '/home/hao01/pytorch/aten/src'],
+        #  library_dirs = ['/home/hao01/torch_extension/lib/python3.6/site-packages/torch/lib'],
         libraries = ["torch"]),
     ],
     cmdclass={
